e3ae03a02f224b0437fbc9c9dcf655ab5b45a6c0
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-08-29 17:28:03 +0300

    Creating page object done

--- src/chapter4/creating_page_object.py
+++ src/chapter4/creating_page_object.py
@@ -1,13 +1,16 @@
 from selenium.webdriver import Firefox
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.firefox.webdriver import WebDriver
 from webdriver_manager.firefox import GeckoDriverManager
-
-browser = Firefox(executable_path=GeckoDriverManager().install())
 
 
 class TrainingGroundPage:
     def __init__(self, driver: WebDriver) -> None:
         self.driver = driver
+        self.url = "https://techstepacademy.com/training-ground"
+
+    def go(self):
+        self.driver.get(self.url)
 
     def type_into_input(self, text: str) -> None:
         input_field = self.driver.find_element_by_id("ipt1")
@@ -21,3 +24,27 @@ class TrainingGroundPage:
     def click_button_1(self) -> None:
         button = self.driver.find_element_by_id("b1")
         button.click()
+
+    def close(self) -> None:
+        self.driver.close()
+
+    def accept_alert(self) -> None:
+        Alert(self.driver).accept()
+
+
+def main() -> None:
+    browser = Firefox(executable_path=GeckoDriverManager().install())
+    page = TrainingGroundPage(browser)
+    page.go()
+
+    page.type_into_input("it worked")
+    assert page.get_input_text() == "it worked"
+
+    page.click_button_1()
+    page.accept_alert()
+
+    page.close()
+
+
+if __name__ == "__main__":
+    main()
